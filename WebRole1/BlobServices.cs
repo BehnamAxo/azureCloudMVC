748be0d2a3fd5a4de6c0f84bd9b047b6734ffb37
Behnam Amiri <behnama@axosoft.com>
2018-05-15 12:17:05 -0700

    azure blob storage

---
+++ WebRole1/BlobServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System.Configuration;
+
+namespace WebRole1
+{
+	public class BlobServices
+	{
+		public CloudBlobContainer GetCloudBlobContainer()
+		{
+			string connString = "DefaultEndpointsProtocol=https;xxxxx";
+			string destContainer = "xxxx";
+
+			// Get a reference to the storage account  
+			CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connString);
+			CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+			CloudBlobContainer blobContainer = blobClient.GetContainerReference(destContainer);
+
+			if (blobContainer.CreateIfNotExists())
+			{
+				blobContainer.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+			}
+			return blobContainer;
+		}
+
+	}
+}
